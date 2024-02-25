cd658b854ec23cc32d3fdaec3ad4b228ae0b9088
Wale <onafesowale@gmail.com>
2024-02-25 20:59:51 +0100

    Http Request

---
+++ src/HB.Domain/Models/Http/HttpRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace HB.Domain.Models.Http;
+public class HttpRequest
+{
+    public string Uri { get; set; }
+    public HttpMethod Method { get; set; }
+    public Dictionary<string, string> Headers { get; } = new();
+}
+
+public class HttpRequest<TBody> : HttpRequest
+{
+    public TBody Body { get; set; }
+}

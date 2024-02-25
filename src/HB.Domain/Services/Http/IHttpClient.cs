34d181efb8da05d94f678bc2dd67b2f58268faee
Wale <onafesowale@gmail.com>
2024-02-25 20:59:42 +0100

    IHttpClient

---
+++ src/HB.Domain/Services/Http/IHttpClient.cs
@@ -0,0 +1,11 @@
+﻿using HB.Domain.Models.Http;
+using HB.Domain.Shared;
+
+namespace HB.Domain.Services.Http;
+public interface IHttpClient
+{
+    Task<Result<TResult, TErrorResult>> SendAsync<TBody, TResult, TErrorResult>(HttpRequest<TBody> model)
+        where TErrorResult : BaseError;
+    Task<Result<TResult, TErrorResult>> SendAsync<TResult, TErrorResult>(HttpRequest model)
+        where TErrorResult : BaseError;
+}

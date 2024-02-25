f480a10f59acbc29ff3c793306bfa9b8c00453c7
Wale <onafesowale@gmail.com>
2024-02-25 20:58:20 +0100

    Result class

---
+++ src/HB.Domain/Shared/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace HB.Domain.Shared;
+public class Result
+{
+    public Result()
+    {
+        IsSuccess = true;
+        Error = Error.None;
+    }
+
+    public Result(Error error)
+    {
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; private set; }
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; private set; }
+    public static Result Success => new();
+}

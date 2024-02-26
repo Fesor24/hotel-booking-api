e127ef23c649176980a573a8f1d294e7589aa4ce
Wale <onafesowale@gmail.com>
2024-02-26 17:53:00 +0100

    Error

--- src/HB.Domain/Shared/Error.cs
+++ src/HB.Domain/Shared/Error.cs
@@ -1,17 +1,16 @@
-﻿using System.Text.Json;
-
-namespace HB.Domain.Shared;
+﻿namespace HB.Domain.Shared;
 public class Error : BaseError
 {
-    public readonly static Error None = new(string.Empty, string.Empty);
+    public readonly static Error None = new(string.Empty, string.Empty, string.Empty);
 
     public Error() : base()
     {
         
     }
 
-    public Error(string code, string message) : base(code, message)
+    public Error(string code, string message, string details) : base(code, message, details)
     {
+        
     }
 }
 

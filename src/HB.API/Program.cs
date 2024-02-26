a10edb38d236ba0aa43ddff854a1c1c719d07570
Wale <onafesowale@gmail.com>
2024-02-26 05:18:52 +0100

    program

--- src/HB.API/Program.cs
+++ src/HB.API/Program.cs
@@ -1,6 +1,36 @@
+using HB.Application;
+using HB.Application.Features.Hotel.Queries.CheckStatus;
+using HB.Infrastructure;
+using HB.Infrastructure.Configurations;
+using MediatR;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSerilog();
+
+builder.Services.AddInfrastructureServices()
+    .AddApplicationServices();
+
+builder.Services.Configure<HotelBedConfiguration>(
+    builder.Configuration.GetSection(HotelBedConfiguration.CONFIGURATION_NAME));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("api/hotel/status", async (MediatR.ISender sender) =>
+{
+    var res = await sender.Send(new CheckStatusRequest());
+
+    return Results.Ok(res.Value);
+});
+
+app.UseSerilogRequestLogging();
 
 app.Run();

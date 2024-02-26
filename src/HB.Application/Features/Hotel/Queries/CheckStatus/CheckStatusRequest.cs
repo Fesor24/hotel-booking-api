cf2f3af14d3791b53806b953b895da5dd3d2d9b7
Wale <onafesowale@gmail.com>
2024-02-26 05:19:11 +0100

    check status request

---
+++ src/HB.Application/Features/Hotel/Queries/CheckStatus/CheckStatusRequest.cs
@@ -0,0 +1,6 @@
+﻿using HB.Domain.Models.HotelBed;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Queries.CheckStatus;
+public record CheckStatusRequest : IRequest<Result<object, HotelBedErrorResponse>>; 

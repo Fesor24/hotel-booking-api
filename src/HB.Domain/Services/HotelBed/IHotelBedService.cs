599ce057c89e3d2581ac5601f2e060e0ec9d78c3
Wale <onafesowale@gmail.com>
2024-02-26 05:20:04 +0100

    hotel be service

---
+++ src/HB.Domain/Services/HotelBed/IHotelBedService.cs
@@ -0,0 +1,8 @@
+﻿using HB.Domain.Models.HotelBed;
+using HB.Domain.Shared;
+
+namespace HB.Domain.Services.HotelBed;
+public interface IHotelBedService
+{
+    Task<Result<object, HotelBedErrorResponse>> CheckStatus();
+}
---
+++ src/HB.Infrastructure/Services/HotelBed/HotelBedService.cs
@@ -0,0 +1,56 @@
+﻿using HB.Domain.Models.HotelBed;
+using HB.Domain.Models.Http;
+using HB.Domain.Services.HotelBed;
+using HB.Domain.Services.Http;
+using HB.Domain.Shared;
+using HB.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HB.Infrastructure.Services.HotelBed;
+public class HotelBedService : IHotelBedService
+{
+    private readonly IHttpClient _httpClient;
+    private readonly HotelBedConfiguration _hotelBedConfig;
+
+    public HotelBedService(IHttpClient httpClient, IOptions<HotelBedConfiguration> hotelBedConfig)
+    {
+        _httpClient = httpClient;
+        _hotelBedConfig = hotelBedConfig.Value;
+    }
+
+    public async Task<Result<object, HotelBedErrorResponse>> CheckStatus()
+    {
+        string url = _hotelBedConfig.Url + "/hotel-api/1.0/status";
+
+        var req = new HttpRequest();
+
+        req.Headers.Add("Api-key", _hotelBedConfig.ApiKey);
+        req.Headers.Add("X-Signature", GetComputedHashedSignature());
+
+        req.Uri = url;
+        req.Method = HttpMethod.Get;
+
+        var res = await _httpClient.SendAsync<object, HotelBedErrorResponse>(req);
+
+        return res;
+    }
+
+    private string GetComputedHashedSignature()
+    {
+        string valueToBeHashed = _hotelBedConfig.ApiKey + _hotelBedConfig.Secret +
+            DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+
+        byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(valueToBeHashed));
+
+        StringBuilder sb = new();
+
+        for(int i = 0; i < bytes.Length; i++)
+        {
+            sb.Append(bytes[i].ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+}

14725888a5195c2c21e520c990db52c748179094
Wale <onafesowale@gmail.com>
2024-02-26 17:50:31 +0100

    hotel endpoints

---
+++ src/HB.API/Endpoints/HotelEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using HB.API.Extensions;
+using HB.API.Shared;
+using HB.Application.Features.Hotel.Queries.CheckStatus;
+using HB.Application.Features.Hotel.Queries.Search;
+using HB.Domain.Models.HotelBed;
+
+namespace HB.API.Endpoints;
+
+public class HotelEndpoints : IEndpoint
+{
+    public void Register(WebApplication app)
+    {
+        const string ENDPOINT = "Hotel";
+
+        app.MediatorGet<CheckStatusRequest, HotelBedStatusResponse>(ENDPOINT, "status");
+        app.MediatorPost<HotelSearchRequest, object>(ENDPOINT, "search");
+    }
+}

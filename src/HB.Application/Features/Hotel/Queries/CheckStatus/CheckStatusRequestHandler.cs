4e51b75ac72aad4d9980abd91b0724aabc23029d
Wale <onafesowale@gmail.com>
2024-02-26 05:19:23 +0100

    check status request handler

---
+++ src/HB.Application/Features/Hotel/Queries/CheckStatus/CheckStatusRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using HB.Domain.Models.HotelBed;
+using HB.Domain.Services.HotelBed;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Queries.CheckStatus;
+internal sealed class CheckStatusRequestHandler : IRequestHandler<CheckStatusRequest,
+    Result<object, HotelBedErrorResponse>>
+{
+    private readonly IHotelBedService _hotelBedService;
+
+    public CheckStatusRequestHandler(IHotelBedService hotelBedService)
+    {
+        _hotelBedService = hotelBedService;
+    }
+
+    public async Task<Result<object, HotelBedErrorResponse>> Handle(CheckStatusRequest request, 
+        CancellationToken cancellationToken)
+    {
+        return await _hotelBedService.CheckStatus();
+    }
+}

5c5a2c309b91802fb273cf7c7017e65e1c270b78
Wale <onafesowale@gmail.com>
2024-02-26 17:50:51 +0100

    mediatr extensions

---
+++ src/HB.API/Extensions/MediatrExtensions.cs
@@ -0,0 +1,48 @@
+﻿using HB.API.Shared;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.API.Extensions;
+
+internal static class MediatrExtensions
+{
+    internal static void MediatorGet<TRequest, TResponse>(this WebApplication app, string endpointGroup, string route)
+        where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        route = $"/api/{endpointGroup}/{route}";
+
+        app.MapGet(route, HandleGetRequests<TRequest, TResponse>)
+            .WithGroupName(endpointGroup)
+            .Produces<TResponse>();
+    }
+
+    internal static void MediatorPost<TRequest, TResponse>(this WebApplication app, string endpointGroup, string route)
+        where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        route = $"/api/{endpointGroup}/{route}";
+
+        app.MapPost(route, HandlePostRequests<TRequest, TResponse>)
+            .WithGroupName(endpointGroup)
+            .Produces<TResponse>();
+    }
+
+    private static async Task<IResult> HandleGetRequests<TRequest, TResponse>(ISender sender, 
+        [AsParameters]TRequest request)
+        where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        var res = await sender.Send(request);
+
+        return res.Match(val => Results.Ok(new ApiResponse<TResponse>(val)), 
+            err => Results.BadRequest(new ApiResponse(err)));
+    }
+
+    private static async Task<IResult> HandlePostRequests<TRequest, TResponse>(ISender sender,
+        TRequest request)
+        where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        var res = await sender.Send(request);
+
+        return res.Match(val => Results.Ok(new ApiResponse<TResponse>(val)),
+            err => Results.BadRequest(new ApiResponse(err)));
+    }
+}

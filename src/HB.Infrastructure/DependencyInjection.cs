abf9364d26f1492dd7d75996a93a6cc2b234a5a7
Wale <onafesowale@gmail.com>
2024-02-26 05:20:33 +0100

    Dependency injection

---
+++ src/HB.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using HB.Domain.Services.HotelBed;
+using HB.Domain.Services.Http;
+using HB.Infrastructure.Services.HotelBed;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HB.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddHttpClient();
+
+        services.AddScoped<IHttpClient, Services.Http.HttpClient>();
+
+        services.AddScoped<IHotelBedService, HotelBedService>();
+
+        return services;
+    }
+}

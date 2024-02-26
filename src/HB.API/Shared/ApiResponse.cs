4187c7d16f367d927d80d9aabfcf4384f99a92ac
Wale <onafesowale@gmail.com>
2024-02-26 17:51:25 +0100

    api response

---
+++ src/HB.API/Shared/ApiResponse.cs
@@ -0,0 +1,36 @@
+﻿using HB.Domain.Shared;
+
+namespace HB.API.Shared;
+
+public class ApiResponse
+{
+    public ApiResponse()
+    {
+        IsSuccess = true;
+    }
+
+    public ApiResponse(Error error)
+    {
+        Error = error;
+        IsSuccess = false;
+    }
+
+    public bool IsSuccess { get; private set; }
+
+    public Error Error { get; set; }
+
+    public static ApiResponse Success => new();
+
+}
+
+public class ApiResponse<TResponse> : ApiResponse
+{
+    public ApiResponse(TResponse response) : base()
+    {
+        Data = response;
+    }
+
+    public TResponse Data { get; set; }
+
+    public static implicit operator ApiResponse<TResponse>(TResponse response) => new(response);
+}

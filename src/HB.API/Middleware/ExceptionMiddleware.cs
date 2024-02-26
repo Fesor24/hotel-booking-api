bc7fe2d85a64b42288abc6230c94219cf966fc5d
Wale <onafesowale@gmail.com>
2024-02-26 17:51:00 +0100

    exception middleware

---
+++ src/HB.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using HB.API.Shared;
+using HB.Domain.Shared;
+using System.Text.Json;
+
+namespace HB.API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env)
+    {
+        _next = next;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var error = _env.IsProduction() ? new Error("500", ex.Message, string.Empty) :
+                new Error("500", ex.Message, ex.StackTrace);
+
+            var apiResponse = new ApiResponse(error);
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            string response = JsonSerializer.Serialize(apiResponse, jsonOptions);
+
+            await context.Response.WriteAsync(response);
+        }
+    }
+}

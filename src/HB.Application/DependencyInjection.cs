5c24c749bbff64bbed8ed4583cf67dbc264a1fd8
Wale <onafesowale@gmail.com>
2024-02-26 05:19:04 +0100

    Dependency injection

---
+++ src/HB.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HB.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}

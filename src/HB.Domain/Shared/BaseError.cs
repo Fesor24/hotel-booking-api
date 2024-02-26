3008e98c5483652bc71aea000ba5f7f6fdc1e0ee
Wale <onafesowale@gmail.com>
2024-02-26 17:52:49 +0100

    Base error

--- src/HB.Domain/Shared/BaseError.cs
+++ src/HB.Domain/Shared/BaseError.cs
@@ -5,14 +5,17 @@ public abstract class BaseError
     {
         Code = "404";
         Message = "An error occurred";
+        Details = "";
     }
 
-    protected BaseError(string code, string message)
+    protected BaseError(string code, string message, string details)
     {
         Code = code;
         Message = message;
+        Details = details;
     }
 
     public string Code { get; protected set; }
     public string Message { get; set; }
+    public string Details { get; set; }
 }

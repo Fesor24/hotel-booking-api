15fd01d8686d99a8edc1be4a67a16a146adbb0c9
Wale <onafesowale@gmail.com>
2024-02-26 17:52:26 +0100

    Hotel bed response

---
+++ src/HB.Domain/Models/HotelBed/HotelBedResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HB.Domain.Models.HotelBed;
+public class HotelBedStatusResponse
+{
+    [JsonPropertyName("auditData")]
+    public Audit AuditData { get; set; }
+    [JsonPropertyName("status")]
+    public string Status { get; set; }
+    public class Audit
+    {
+        [JsonPropertyName("timestamp")]
+        public string TimeStamp { get; set; }
+    }
+}

f4df32ba526c6d402bb6c41fd6e2154e7de6a7f1
Wale <onafesowale@gmail.com>
2024-02-26 05:20:45 +0100

    Http client

--- src/HB.Infrastructure/Services/Http/HttpClient.cs
+++ src/HB.Infrastructure/Services/Http/HttpClient.cs
@@ -36,12 +36,13 @@ public class HttpClient : IHttpClient
             response = await client.SendAsync(new HttpRequestMessage
             {
                 Method = model.Method,
-                RequestUri = new Uri(model.Uri)
+                RequestUri = new Uri(model.Uri),
+                Content = JsonContent.Create(model.Body)
             });
         }
         catch (Exception ex)
         {
-
+            throw;
         }
 
         if (response.IsSuccessStatusCode)
@@ -63,8 +64,52 @@ public class HttpClient : IHttpClient
         }
     }
 
-    public Task<Result<TResult, TErrorResult>> SendAsync<TResult, TErrorResult>(HttpRequest model) where TErrorResult : BaseError
+    public async Task<Result<TResult, TErrorResult>> SendAsync<TResult, TErrorResult>(HttpRequest model) 
+        where TErrorResult : BaseError
     {
-        throw new NotImplementedException();
+        using var client = _httpClientFactory.CreateClient();
+
+        HttpResponseMessage response = new();
+
+        var jsonOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+        };
+
+        foreach (var header in model.Headers)
+        {
+            client.DefaultRequestHeaders.Add(header.Key, header.Value);
+        }
+
+        try
+        {
+            response = await client.SendAsync(new HttpRequestMessage
+            {
+                Method = model.Method,
+                RequestUri = new Uri(model.Uri)
+            });
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
+        if (response.IsSuccessStatusCode)
+        {
+            var res = await response.Content.ReadFromJsonAsync<TResult>(jsonOptions);
+
+            return res;
+        }
+        else
+        {
+            string errorMessage = response.ReasonPhrase;
+
+            var err = await response.Content.ReadFromJsonAsync<TErrorResult>(jsonOptions);
+
+            if (err is not null)
+                err.Message = errorMessage;
+
+            return err;
+        }
     }
 }

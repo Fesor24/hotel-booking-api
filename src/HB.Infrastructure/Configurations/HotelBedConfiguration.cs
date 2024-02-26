b9a747a86900c2d24723eb84149850fad3ef1262
Wale <onafesowale@gmail.com>
2024-02-26 05:20:22 +0100

    Hotel bed configuration

---
+++ src/HB.Infrastructure/Configurations/HotelBedConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace HB.Infrastructure.Configurations;
+public class HotelBedConfiguration
+{
+    public const string CONFIGURATION_NAME = "HotelBed";
+    public string ApiKey { get; set; }
+    public string Secret { get; set; }
+    public string Url { get; set; }
+}

b93b567b977b37c2f07993d93c061121fdb321c6
Wale <onafesowale@gmail.com>
2024-02-26 17:52:06 +0100

    Hotel search request

---
+++ src/HB.Application/Features/Hotel/Queries/Search/HotelSearchRequest.cs
@@ -0,0 +1,19 @@
+﻿using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Queries.Search;
+public record HotelSearchRequest(
+    HotelSearchDuration Duration,
+    HotelSearchOccupants Occupants
+    ) : IRequest<Result<object, Error>>;
+
+public record HotelSearchDuration(
+    DateTime CheckIn,
+    DateTime CheckOut
+    );
+
+public record HotelSearchOccupants(
+    int Adults = 1,
+    int Rooms = 1,
+    int Children = 0
+    );

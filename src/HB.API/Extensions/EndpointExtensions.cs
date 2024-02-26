93d2928314fb4500ee8ad78bde3770ea265c648e
Wale <onafesowale@gmail.com>
2024-02-26 17:50:43 +0100

    endpoint extensions

---
+++ src/HB.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,20 @@
+﻿using HB.API.Shared;
+using System.Reflection;
+
+namespace HB.API.Extensions;
+
+public static class EndpointExtensions
+{
+    public static void RegisterEndpoints(this WebApplication app)
+    {
+        var endpoints = Assembly.GetExecutingAssembly()
+            .GetExportedTypes()
+            .Where(x => x.IsAssignableTo(typeof(IEndpoint)) && !x.IsInterface && !x.IsAbstract)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpoint>()
+            .ToList();
+
+        foreach (var endpoint in endpoints)
+            endpoint.Register(app);
+    }
+}

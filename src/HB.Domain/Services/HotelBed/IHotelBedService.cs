9ea67425147c5d101aa7cc9ab3800467a6f238f6
Wale <onafesowale@gmail.com>
2024-03-01 16:14:47 +0100

    replace response obj

--- src/HB.Domain/Services/HotelBed/IHotelBedService.cs
+++ src/HB.Domain/Services/HotelBed/IHotelBedService.cs
@@ -5,5 +5,5 @@ namespace HB.Domain.Services.HotelBed;
 public interface IHotelBedService
 {
     Task<Result<HotelBedStatusResponse, HotelBedErrorResponse>> CheckStatus();
-    Task<Result<object, HotelBedErrorResponse>> Search(HotelSearch search);
+    Task<Result<HotelSearchResponse, HotelBedErrorResponse>> Search(HotelSearch search);
 }

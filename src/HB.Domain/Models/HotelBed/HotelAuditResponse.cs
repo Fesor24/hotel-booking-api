1c3b2058af507ee0f9744ba0e39159011883ba0b
Wale <onafesowale@gmail.com>
2024-03-03 05:25:38 +0100

    hotel audit response

---
+++ src/HB.Domain/Models/HotelBed/HotelAuditResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace HB.Domain.Models.HotelBed;
+public class HotelAuditResponse
+{
+    public string ProcessTime { get; set; }
+    public string Timestamp { get; set; }
+    public string RequestHost { get; set; }
+    public string ServerId { get; set; }
+    public string Environment { get; set; }
+    public string Release { get; set; }
+    public string Token { get; set; }
+    public string Internal { get; set; }
+}

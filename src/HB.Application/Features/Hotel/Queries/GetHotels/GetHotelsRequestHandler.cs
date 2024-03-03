31ca959cf340c5c29ad93f395072f5bb21c91bbf
Wale <onafesowale@gmail.com>
2024-03-03 04:24:48 +0100

    get hotels request handler

---
+++ src/HB.Application/Features/Hotel/Queries/GetHotels/GetHotelsRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using HB.Domain.Services.HotelBed;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Queries.GetHotels;
+internal sealed class GetHotelsRequestHandler : IRequestHandler<GetHotelsRequest, Result<object, Error>>
+{
+    private readonly IHotelBedService _hotelBedService;
+
+    public GetHotelsRequestHandler(IHotelBedService hotelBedService)
+    {
+        _hotelBedService = hotelBedService;
+    }
+
+    public async Task<Result<object, Error>> Handle(GetHotelsRequest request, CancellationToken cancellationToken)
+    {
+        var res = await _hotelBedService.GetHotels();
+
+        if (res.IsFailure)
+        {
+            string errorDetails = string.IsNullOrWhiteSpace(res.ErrorResult.Error) ? res.ErrorResult.Details :
+                res.ErrorResult.Error;
+
+            return new Error("400", res.ErrorResult.Message, errorDetails);
+        }
+
+        return res.Value;
+    }
+}

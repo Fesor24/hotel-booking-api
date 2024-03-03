c6c6841e77a3d2b5bf11e9dfbcd93fa9f2151404
Wale <onafesowale@gmail.com>
2024-03-03 05:25:48 +0100

    hotel location response

---
+++ src/HB.Domain/Models/HotelBed/HotelLocationResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace HB.Domain.Models.HotelBed;
+public class HotelLocationResponse
+{
+    public int From { get; set; }
+    public int To { get; set; }
+    public int Total { get; set; }
+
+    public HotelAuditResponse AuditData { get; set; } = new();
+    public List<Country> Countries { get; set; } = new();
+
+    public class Country
+    {
+        public string Code { get; set; }
+        public string IsoCode { get; set; }
+        public CountryDescription Description { get; set; } = new();
+        public List<State> States { get; set; } = new();
+
+        public class CountryDescription
+        {
+            public string Content { get; set; }
+        }
+
+        public class State
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}

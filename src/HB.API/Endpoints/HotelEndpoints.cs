153a8c422e0ff28672a14819e34e6b0ff7b8800e
Wale <onafesowale@gmail.com>
2024-03-04 04:50:43 +0100

    added endpoints

--- src/HB.API/Endpoints/HotelEndpoints.cs
+++ src/HB.API/Endpoints/HotelEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using HB.API.Extensions;
 using HB.API.Shared;
+using HB.Application.Features.Hotel.Commands.BookingConfirmation;
+using HB.Application.Features.Hotel.Queries.CheckRates;
 using HB.Application.Features.Hotel.Queries.CheckStatus;
 using HB.Application.Features.Hotel.Queries.GetHotels;
 using HB.Application.Features.Hotel.Queries.GetLocations;
@@ -15,8 +17,10 @@ public class HotelEndpoints : IEndpoint
         const string ENDPOINT = "Hotel";
 
         app.MediatorGet<CheckStatusRequest, HotelBedStatusResponse>(ENDPOINT, "status");
-        app.MediatorPost<HotelSearchRequest, HotelSearchResponse>(ENDPOINT, "search");
+        app.MediatorPost<HotelSearchRequest, HotelSearchResponse.HotelData>(ENDPOINT, "search");
         app.MediatorGet<GetHotelsRequest, object>(ENDPOINT, "");
         app.MediatorGet<GetLocationsRequest, HotelLocationResponse>(ENDPOINT, "locations");
+        app.MediatorPost<CheckRateRequest, HotelRateResponse.HotelRate>(ENDPOINT, "rates");
+        app.MediatorPost<HotelBookingConfirmationRequest, HotelBookingConfirmationResponse.HotelBooking>(ENDPOINT, "booking");
     }
 }

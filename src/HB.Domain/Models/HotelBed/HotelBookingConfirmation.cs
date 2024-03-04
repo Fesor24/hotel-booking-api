20a10cf481ee818bb7b4a0ade14204871fb5842b
Wale <onafesowale@gmail.com>
2024-03-04 04:52:02 +0100

    hotel booking confirmation model

---
+++ src/HB.Domain/Models/HotelBed/HotelBookingConfirmation.cs
@@ -0,0 +1,21 @@
+﻿namespace HB.Domain.Models.HotelBed;
+public class HotelBookingConfirmation
+{
+    public BookingHolder Holder { get; set; } = new();
+    public string ClientReference { get; set; }
+    public List<Room> Rooms { get; set; } = new();
+    public string Remark { get; set; }
+    public string Tolerance { get; set; } = "80.00";
+
+    public class BookingHolder
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    public class Room
+    {
+        public string RateKey { get; set; }
+
+    }
+}

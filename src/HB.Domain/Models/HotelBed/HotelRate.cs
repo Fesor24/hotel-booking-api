611442822ce9b2e5b621541eb0787d6bca98dcff
Wale <onafesowale@gmail.com>
2024-03-04 04:52:33 +0100

    hotel rate model

---
+++ src/HB.Domain/Models/HotelBed/HotelRate.cs
@@ -0,0 +1,10 @@
+﻿namespace HB.Domain.Models.HotelBed;
+public class HotelRate
+{
+    public List<Room> Rooms { get; set; } = new();
+
+    public class Room
+    {
+        public string RateKey { get; set; }
+    }
+}

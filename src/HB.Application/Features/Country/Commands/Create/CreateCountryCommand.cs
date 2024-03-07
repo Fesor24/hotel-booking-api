24ecc81aba3f034f44d9b971f93574c12c47fe8f
Wale <onafesowale@gmail.com>
2024-03-07 12:10:40 +0100

    create country command

---
+++ src/HB.Application/Features/Country/Commands/Create/CreateCountryCommand.cs
@@ -0,0 +1,5 @@
+﻿using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Country.Commands.Create;
+public record CreateCountryCommand() : IRequest<Result<string, Error>>;
---
+++ src/HB.Application/Features/Country/Commands/Create/CreateCountryCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using HB.Domain.Entity.CountryAggregate;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Country.Commands.Create;
+internal sealed class CreateCountryCommandHandler : IRequestHandler<CreateCountryCommand, Result<string, Error>>
+{
+    private readonly ICountryRepository _countryRepository;
+
+    public CreateCountryCommandHandler(ICountryRepository countryRepository)
+    {
+        _countryRepository = countryRepository;
+    }
+
+    public async Task<Result<string, Error>> Handle(CreateCountryCommand request, CancellationToken cancellationToken)
+    {
+        await _countryRepository.AddAsync(new Domain.Entity.CountryAggregate.Country
+        {
+            Name = "Nigeria",
+            Code = "NG",
+        });
+
+        return "";
+    }
+}

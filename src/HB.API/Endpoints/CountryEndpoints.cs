dc60dc710ae521d704988f9df4fde28a5469c2ac
Wale <onafesowale@gmail.com>
2024-03-07 12:09:51 +0100

    country endpoints

---
+++ src/HB.API/Endpoints/CountryEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using HB.API.Extensions;
+using HB.API.Shared;
+using HB.Application.Features.Country.Commands.Create;
+using HB.Application.Features.Country.Queries;
+
+namespace HB.API.Endpoints;
+
+public class CountryEndpoints : IEndpoint
+{
+    public void Register(WebApplication app)
+    {
+        const string ENDPOINT = "Country";
+
+        app.MediatorGet<GetCountriesRequest, List<GetCountryResponse>>(ENDPOINT, "list");
+        app.MediatorGet<CreateCountryCommand, string>(ENDPOINT, "");
+    }
+}

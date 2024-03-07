cbd53baec2e8e29a77610922a681c7eda81ff9ca
Wale <onafesowale@gmail.com>
2024-03-07 14:44:23 +0100

    get facilities functionality

---
+++ src/HB.Application/Features/Hotel/Queries/GetFacilities/GetFacilitiesRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using HB.Domain.Services.HotelBed;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Queries.GetFacilities;
+internal sealed class GetFacilitiesRequestHandler : IRequestHandler<GetFacilitiesRequest, Result<object, Error>>
+{
+    private readonly IHotelBedService _hotelBedService;
+
+    public GetFacilitiesRequestHandler(IHotelBedService hotelBedService)
+    {
+        _hotelBedService = hotelBedService;
+    }
+
+    public async Task<Result<object, Error>> Handle(GetFacilitiesRequest request, CancellationToken cancellationToken)
+    {
+        var res = await _hotelBedService.GetFacilities(request.From, request.To);
+
+        if (res.IsFailure)
+        {
+            string errorMessage = string.IsNullOrWhiteSpace(res.ErrorResult.Error) ? res.ErrorResult.Details :
+                res.ErrorResult.Error;
+
+            return new Error("404", res.Error.Message, errorMessage);
+        }
+
+        return res.Value;
+    }
+}

b302c5e665d199cca87077492af777aa9e03ad9f
Wale <onafesowale@gmail.com>
2024-03-07 12:13:33 +0100

    generic repository

---
+++ src/HB.Domain/Primitives/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace HB.Domain.Primitives;
+public interface IGenericRepository<TEntity> where TEntity: BaseEntity
+{
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+    Task<List<TEntity>> GetAllAsync();
+    Task<List<TEntity>> GetAllByValueAsync<TField>
+        (Expression<Func<TEntity, TField>> criteria, TField value);
+}
---
+++ src/HB.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using HB.Domain.Primitives;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace HB.Infrastructure.Repository;
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : Entity
+{
+    private readonly IMongoCollection<TEntity> _collection;
+
+    public GenericRepository(IMongoCollection<TEntity> collection) => _collection = collection;
+
+    public async Task AddAsync(TEntity entity) => 
+        await _collection.InsertOneAsync(entity);
+
+    public async Task AddRangeAsync(IEnumerable<TEntity> entities) =>
+        await _collection.InsertManyAsync(entities);
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        var filter = Builders<TEntity>.Filter.Empty;
+
+        return await _collection.Find(filter)
+            .ToListAsync();
+    }
+
+    public async Task<List<TEntity>> GetAllByValueAsync<TField>
+        (Expression<Func<TEntity, TField>> criteria, TField value)
+    {
+        var filter = Builders<TEntity>.Filter
+            .Eq(criteria, value);
+
+        return await _collection.Find(filter)
+            .ToListAsync();
+    }
+}

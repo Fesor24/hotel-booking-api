c9f1694c567d35e473bfebf7d07248eb62126636
Wale <onafesowale@gmail.com>
2024-03-07 12:13:02 +0100

    entity

---
+++ src/HB.Domain/Primitives/Entity.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HB.Domain.Primitives;
+[BsonIgnoreExtraElements]
+public abstract class Entity
+{
+    [BsonId]
+    //[BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+}

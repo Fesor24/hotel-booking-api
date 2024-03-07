49d052f34e26089ab6eaaeeb44bee6cc4ca384a0
Wale <onafesowale@gmail.com>
2024-03-07 14:44:50 +0100

    hotel entity

---
+++ src/HB.Domain/Entity/HotelAggregate/Hotel.cs
@@ -0,0 +1,25 @@
+﻿namespace HB.Domain.Entity.HotelAggregate;
+public class Hotel : BaseEntity
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string CountryCode { get; set; }
+    public string StateCode { get; set; }
+    public string DestinationCode { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Email { get; set; }
+    public string License { get; set; }
+    public List<PhoneContact> Contact { get; set; }
+    public List<string> Images { get; set; }
+    public int Ranjing { get; set; }
+
+    public class PhoneContact
+    {
+        public string PhoneNumber { get; set; }
+        public string PhoneType { get; set; }
+    }
+   
+}

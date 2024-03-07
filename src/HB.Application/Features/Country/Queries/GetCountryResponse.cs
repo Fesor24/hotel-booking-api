9b12d4167b0e14c5c9edfe870c8064d0f6ba6e4c
Wale <onafesowale@gmail.com>
2024-03-07 14:44:10 +0100

    added prop

--- src/HB.Application/Features/Country/Queries/GetCountryResponse.cs
+++ src/HB.Application/Features/Country/Queries/GetCountryResponse.cs
@@ -1,6 +1,7 @@
 ﻿namespace HB.Application.Features.Country.Queries;
 public class GetCountryResponse
 {
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Code { get; set; }
 }

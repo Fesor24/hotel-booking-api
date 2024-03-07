dbe2d3687ce1930d0d0c7f9111206ba7ead008fa
Wale <onafesowale@gmail.com>
2024-03-07 14:45:13 +0100

    added methods to service

--- src/HB.Domain/Services/HotelBed/IHotelBedService.cs
+++ src/HB.Domain/Services/HotelBed/IHotelBedService.cs
@@ -6,8 +6,10 @@ public interface IHotelBedService
 {
     Task<Result<HotelBedStatusResponse, HotelBedErrorResponse>> CheckStatus();
     Task<Result<HotelSearchResponse, HotelBedErrorResponse>> Search(HotelSearch search);
-    Task<Result<object, HotelBedErrorResponse>> GetHotels(int from, int to);
+    Task<Result<HotelsResponse, HotelBedErrorResponse>> GetHotels(int from, int to);
     Task<Result<HotelLocationResponse, HotelBedErrorResponse>> GetLocations(int from, int to);
     Task<Result<HotelRateResponse, HotelBedErrorResponse>> CheckRates(HotelRate rate);
-    Task<Result<HotelBookingConfirmationResponse, HotelBedErrorResponse>> ConfirmBooking(HotelBookingConfirmation confirmation);
+    Task<Result<HotelBookingConfirmationResponse, HotelBedErrorResponse>> 
+        ConfirmBooking(HotelBookingConfirmation confirmation);
+    Task<Result<object, HotelBedErrorResponse>> GetFacilities(int from, int to);
 }

ede15f937559c48d22fa8ff76b3066c6a8387513
Wale <onafesowale@gmail.com>
2024-03-07 12:10:51 +0100

    get countries request

---
+++ src/HB.Application/Features/Country/Queries/GetCountriesRequest.cs
@@ -0,0 +1,5 @@
+﻿using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Country.Queries;
+public record GetCountriesRequest : IRequest<Result<List<GetCountryResponse>, Error>>;
---
+++ src/HB.Application/Features/Country/Queries/GetCountriesRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using HB.Domain.Entity.CountryAggregate;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Country.Queries;
+internal sealed class GetCountriesRequestHandler : IRequestHandler<GetCountriesRequest, 
+    Result<List<GetCountryResponse>, Error>>
+{
+    private readonly ICountryRepository _countryRepository;
+    private readonly IMapper _mapper;
+
+    public GetCountriesRequestHandler(ICountryRepository countryRepository, IMapper mapper)
+    {
+        _countryRepository = countryRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<List<GetCountryResponse>, Error>> Handle(GetCountriesRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var countries = await _countryRepository.GetAllAsync();
+
+        return _mapper.Map<List<GetCountryResponse>>(countries);
+    }
+}

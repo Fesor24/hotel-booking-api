072a6b53390ff2f6ac52649ab6b756077d430bc8
Wale <onafesowale@gmail.com>
2024-03-07 12:12:32 +0100

    country mappings

---
+++ src/HB.Application/Mappings/CountryMappings.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using HB.Application.Features.Country.Queries;
+using HB.Domain.Entity.CountryAggregate;
+
+namespace HB.Application.Mappings;
+public class CountryMappings : Profile
+{
+    public CountryMappings()
+    {
+        CreateMap<Country, GetCountryResponse>();
+    }
+}

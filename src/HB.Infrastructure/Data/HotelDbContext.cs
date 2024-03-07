ef47bfe8ae5cb18ad52dc49f3fa777ffd60073c7
Wale <onafesowale@gmail.com>
2024-03-07 12:14:43 +0100

    Hotel db context

---
+++ src/HB.Infrastructure/Data/HotelDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HB.Domain.Entity.CountryAggregate;
+using MongoDB.Driver;
+
+namespace HB.Infrastructure.Data;
+public class HotelDbContext
+{
+    private readonly IMongoDatabase _db;
+    private IMongoCollection<Country> _country;
+
+    public HotelDbContext(IMongoDatabase db)
+    {
+        _db = db;
+    }
+
+    public IMongoCollection<Country> Country { get
+        {
+            _country ??= _db.GetCollection<Country>(nameof(Country));
+
+            return _country;
+        } 
+    }
+}

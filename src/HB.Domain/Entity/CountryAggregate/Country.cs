5f98dc4e4f517019a75776943b55f2ad2daa4eab
Wale <onafesowale@gmail.com>
2024-03-07 12:12:43 +0100

    country entity

---
+++ src/HB.Domain/Entity/CountryAggregate/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace HB.Domain.Entity.CountryAggregate;
+public sealed class Country : BaseEntity
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public List<State> States { get; set; }
+}
+
+public class State
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+}

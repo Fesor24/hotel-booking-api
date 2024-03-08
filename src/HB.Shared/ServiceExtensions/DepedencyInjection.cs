d31cda72d26eb08c5da5493844b32d55edc4ea74
Wale <onafesowale@gmail.com>
2024-03-08 07:38:01 +0100

    dependency injection

---
+++ src/HB.Shared/ServiceExtensions/DepedencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using HB.Shared.Markers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace HB.Shared.ServiceExtensions;
+public static class DepedencyInjection
+{
+    public static IServiceCollection RegisterServiceWthMarkerInterface(this IServiceCollection services, Assembly assembly)
+    {
+        var scopedTypes = assembly.GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(IScopedService)) && !x.IsInterface && !x.IsAbstract)
+            .ToList();
+
+        foreach (var scopedType in scopedTypes)
+        {
+            var interfaceType = scopedType.GetInterfaces().FirstOrDefault(x => x != typeof(IScopedService));
+
+            if (interfaceType is not null)
+                services.AddScoped(interfaceType, scopedType);
+        }
+
+        var transientTypes = assembly.GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(ITransientService)) && !x.IsInterface && !x.IsAbstract)
+            .ToList();
+
+        foreach (var transientType in transientTypes)
+        {
+            var interfaceType = transientType.GetInterfaces().FirstOrDefault(x => x != typeof(ITransientService));
+
+            if (interfaceType is not null)
+                services.AddTransient(interfaceType, transientType);
+        }
+
+
+        return services;
+    }
+}

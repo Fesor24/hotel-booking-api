739109d8b113aecaba193b6c7b08ef2e67dec609
Wale <onafesowale@gmail.com>
2024-03-08 07:37:04 +0100

    Hotel search entity

---
+++ src/HB.Domain/Entity/HotelSearchAggregate/HotelSearch.cs
@@ -0,0 +1,19 @@
+﻿namespace HB.Domain.Entity.HotelSearchAggregate;
+public class HotelSearch : BaseEntity
+{
+    public string Location { get; set; }
+    public SearchDuration Duration { get; set; }
+    public HotelOccupants Occupants { get; set; }
+    public class SearchDuration
+    {
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+
+    public class HotelOccupants
+    {
+        public int Adults { get; set; }
+        public int Children { get; set; }
+        public int Rooms { get; set; }
+    }
+}

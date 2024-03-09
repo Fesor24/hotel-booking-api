0abaa3b4092fa888054aec4e694e74f5fccc7519
Wale <onafesowale@gmail.com>
2024-03-09 21:36:36 +0100

    added methods

--- src/HB.API/Extensions/EndpointExtensions.cs
+++ src/HB.API/Extensions/EndpointExtensions.cs
@@ -1,20 +1,36 @@
 ﻿using HB.API.Shared;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Reflection;
 
 namespace HB.API.Extensions;
 
 public static class EndpointExtensions
 {
-    public static void RegisterEndpoints(this WebApplication app)
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
     {
-        var endpoints = Assembly.GetExecutingAssembly()
-            .GetExportedTypes()
-            .Where(x => x.IsAssignableTo(typeof(IEndpoint)) && !x.IsInterface && !x.IsAbstract)
-            .Select(Activator.CreateInstance)
-            .Cast<IEndpoint>()
-            .ToList();
+        ServiceDescriptor[] descriptors = assembly.DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+            type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
 
-        foreach (var endpoint in endpoints)
-            endpoint.Register(app);
+        services.TryAddEnumerable(descriptors);
+
+        return services;
+    }
+
+    public static IApplicationBuilder MapEndpoints(this WebApplication app, RouteGroupBuilder? routeBuilder = null)
+    {
+        IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        IEndpointRouteBuilder builder = routeBuilder is null ?
+            app : routeBuilder;
+
+        foreach(var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(builder);
+        }
+
+        return app;
     }
 }

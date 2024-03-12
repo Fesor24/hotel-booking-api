68921833f1e570ef9cbba6c9752cf349aa90642f
Wale <onafesowale@gmail.com>
2024-03-12 17:16:13 +0100

    create hotels

---
+++ src/HB.Application/Features/Hotel/Commands/Create/CreateHotelsCommand.cs
@@ -0,0 +1,5 @@
+﻿using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Commands.Create;
+public sealed record CreateHotelsCommand(int From, int To) : IRequest<Result<bool, Error>>;

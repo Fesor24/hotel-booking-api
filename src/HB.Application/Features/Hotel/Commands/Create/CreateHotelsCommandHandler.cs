cf9ba2955f11691eb71d912efa6f222bd10d37c1
Wale <onafesowale@gmail.com>
2024-03-12 17:16:32 +0100

    create hotels handler

---
+++ src/HB.Application/Features/Hotel/Commands/Create/CreateHotelsCommandHandler.cs
@@ -0,0 +1,70 @@
+﻿using HB.Domain.Entity.HotelAggregate;
+using HB.Domain.Services.HotelBed;
+using HB.Domain.Shared;
+using MediatR;
+
+namespace HB.Application.Features.Hotel.Commands.Create;
+internal sealed class CreateHotelsCommandHandler : IRequestHandler<CreateHotelsCommand, Result<bool, Error>>
+{
+    private readonly IHotelRepository _hotelRepository;
+    private readonly IHotelBedService _hotelBedService;
+
+    public CreateHotelsCommandHandler(IHotelRepository hotelRepository, IHotelBedService hotelBedService)
+    {
+        _hotelRepository = hotelRepository;
+        _hotelBedService = hotelBedService;
+    }
+
+    public async Task<Result<bool, Error>> Handle(CreateHotelsCommand request, CancellationToken cancellationToken)
+    {
+        var res = await _hotelBedService.GetHotels(request.From, request.To);
+
+        if (res.IsFailure)
+        {
+            string errorDetails = string.IsNullOrWhiteSpace(res.ErrorResult.Error) ? res.ErrorResult.Details :
+                res.ErrorResult.Error;
+
+            return new Error("400", res.ErrorResult.Message, errorDetails);
+        }
+
+        List<HotelEntity> hotels = new();
+
+        foreach(var item in res.Value.Hotels)
+        {
+            List<HotelEntity.PhoneContact> contacts = new();
+
+            foreach(var contact in item.Phones)
+            {
+                HotelEntity.PhoneContact cont = new()
+                {
+                    PhoneNumber = contact.PhoneNumber,
+                    PhoneType = contact.PhoneType
+                };
+            }
+
+            HotelEntity hotel = new()
+            {
+                Name = item.Name.Content,
+                Description = item.Description.Content,
+                Address = item.Address.Content,
+                City = item.City.Content,
+                Code = item.Code,
+                Contact = contacts,
+                CountryCode = item.CountryCode,
+                DestinationCode = item.DestinationCode,
+                PostalCode = item.PostalCode,
+                Email = item.Email,
+                Images = item.Images.Select(x => x.Path).ToList(),
+                License = item.License,
+                Ranking = item.Ranking,
+                StateCode = item.StateCode
+            };
+
+            hotels.Add(hotel);
+        }
+
+        await _hotelRepository.AddRangeAsync(hotels);
+
+        return true;
+    }
+}

bd244d6537ec744f893587fbdc6be518ad7745d8
Wale <onafesowale@gmail.com>
2024-03-16 19:30:55 +0100

    modified response

--- src/HB.Application/Features/Hotel/Queries/Search/HotelSearchRequestHandler.cs
+++ src/HB.Application/Features/Hotel/Queries/Search/HotelSearchRequestHandler.cs
@@ -1,26 +1,41 @@
-﻿using HB.Domain.Models.HotelBed;
+﻿using HB.Domain.Entity.HotelAggregate;
+using HB.Domain.Models.HotelBed;
 using HB.Domain.Services.HotelBed;
 using HB.Domain.Shared;
 using MediatR;
 
 namespace HB.Application.Features.Hotel.Queries.Search;
 internal sealed class HotelSearchRequestHandler : IRequestHandler<HotelSearchRequest, 
-    Result<HotelSearchResponse.HotelData, Error>>
+    Result<SearchHotelResponse, Error>>
 {
     private readonly IHotelBedService _hotelBedService;
+    private readonly IHotelRepository _hotelRepository;
 
-    public HotelSearchRequestHandler(IHotelBedService hotelBedService)
+    public HotelSearchRequestHandler(IHotelBedService hotelBedService, IHotelRepository hotelRepository)
     {
         _hotelBedService = hotelBedService;
+        _hotelRepository = hotelRepository;
     }
 
-    public async Task<Result<HotelSearchResponse.HotelData, Error>> Handle(HotelSearchRequest request, 
+    public async Task<Result<SearchHotelResponse, Error>> Handle(HotelSearchRequest request, 
         CancellationToken cancellationToken)
     {
-        HotelSearch hotelSearch = new();
+        if (request.Duration.CheckIn.Date < DateTime.UtcNow.Date)
+            return new Error("404", "Check in must be a future date");
 
         if (request.Duration.CheckIn > request.Duration.CheckOut)
             return new Error("404", "Check in can not be after check out", "Check in can not be after check out");
+
+        HotelSearch hotelSearch = new();
+
+        var countryHotels = await _hotelRepository
+            .GetAllByValueAsync(x => x.CountryCode, request.CountryCode.ToUpper());
+
+        if (!countryHotels.Any())
+            return new Error("404", "No hotel found for country code");
+
+        List<int> countryCodes = countryHotels.Select(x => x.Code)
+            .ToList();
 
         hotelSearch.Occupancies.Add(new HotelSearch.Occupancy
         {
@@ -35,7 +50,7 @@ internal sealed class HotelSearchRequestHandler : IRequestHandler<HotelSearchReq
             CheckOut = request.Duration.CheckOut.ToString("yyyy-MM-dd")
         };
 
-        hotelSearch.Hotels.Hotel = request.HotelCodes;
+        hotelSearch.Hotels.Hotel = countryCodes;
 
         var res = await _hotelBedService.Search(hotelSearch);
 
@@ -47,6 +62,8 @@ internal sealed class HotelSearchRequestHandler : IRequestHandler<HotelSearchReq
             return new Error("404", res.ErrorResult.Message, errorDetails);
         }
 
-        return res.Value.Hotels;
+        SearchHotelResponse response = new(res.Value.Hotels, new SearchMeta(request.Duration, request.Occupants));
+
+        return response;
     }
 }
